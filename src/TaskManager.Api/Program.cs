75c78ade9bc2a7b4072e050f88b84a57d5ab14d4
Glaucio pançardes <gbarizon@outlook.com>
2025-07-15 23:52:23 -0300

    -Subindo Dockerfile, mas a API não está subindo automaticamente, somente o sqlserver, mas se der RUN ela funciona, estou verificando a causa de nao subir automaticamente ainda.
    -docker-compose.yml ok subindo junto
    
    -colocado no program.cs uma parte para executar o migration caso seja necessário

--- src/TaskManager.Api/Program.cs
+++ src/TaskManager.Api/Program.cs
@@ -10,58 +10,81 @@ using TaskManager.Domain.Repositories;
 using TaskManager.Infrastructure.Data;
 using TaskManager.Infrastructure.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<TaskManagerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Repositories
-builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-builder.Services.AddScoped<ITaskCommentRepository, TaskCommentRepository>();
-builder.Services.AddScoped<ITaskHistoryRepository, TaskHistoryRepository>();
-builder.Services.AddScoped<ITaskRepository, TaskRepository>();
-
-//Registrar o repositório de tarefas no MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateProjectHandler>());
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<DeleteProjectHandler>());
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<AddCommentHandler>());
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateTaskHandler>());
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<UpdateTaskHandler>());
-
-//Validators
-builder.Services.AddValidatorsFromAssemblyContaining<CreateTaskDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<AddCommentDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UpdateTaskDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CreateProjectDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<DeleteProjectCommandValidator>();
-
-builder.Services.AddSwaggerGen(options =>
+public partial class Program
 {
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        builder.Services.AddDbContext<TaskManagerDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Repositories
+        builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+        builder.Services.AddScoped<ITaskCommentRepository, TaskCommentRepository>();
+        builder.Services.AddScoped<ITaskHistoryRepository, TaskHistoryRepository>();
+        builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+        //Registrar o repositório de tarefas no MediatR
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateProjectHandler>());
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<DeleteProjectHandler>());
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<AddCommentHandler>());
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateTaskHandler>());
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<UpdateTaskHandler>());
+
+        //Validators
+        builder.Services.AddValidatorsFromAssemblyContaining<CreateTaskDtoValidator>();
+        builder.Services.AddValidatorsFromAssemblyContaining<AddCommentDtoValidator>();
+        builder.Services.AddValidatorsFromAssemblyContaining<UpdateTaskDtoValidator>();
+        builder.Services.AddValidatorsFromAssemblyContaining<CreateProjectDtoValidator>();
+        builder.Services.AddValidatorsFromAssemblyContaining<DeleteProjectCommandValidator>();
+
+        builder.Services.AddSwaggerGen(options =>
+        {
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
+        builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+        });
+
+        var app = builder.Build();
+        app.UseCors("AllowAll");
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        // Adicione esta linha para mapear os controllers:
+        app.MapControllers();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<TaskManagerDbContext>();
+            db.Database.Migrate();
+        }
+
+        app.Run();
+
+    }
 }
 
-app.UseHttpsRedirection();
 
-// Adicione esta linha para mapear os controllers:
-app.MapControllers();
 
-app.Run();
